53a67a26a947042f3bc16fbe7c5b8f08470e9ba3
Dobby <thomas.dobbelaere@uclouvain.be>
2025-11-07 20:16:29 +0100

    new layout

--- tetris/src/ui.rs
+++ tetris/src/ui.rs
@@ -13,18 +13,21 @@ use ratatui::{
     layout::{Constraint, Flex, Layout, Rect},
     style::{Color, Style, Stylize},
     symbols::border,
-    text::Line,
     widgets::{Block, Paragraph, Widget},
 };
 
 const UNIT_X: u16 = 2;
 const UNIT_Y: u16 = 1;
+const TITLE_HEIGHT: u16 = 6;
+const STAT_WIDTH: u16 = 8;
+const STAT_HEIGHT: u16 = 10;
+const TITLE_WIDTH: u16 = 29;
 
-pub const BIG_TETRIS: &str = r#"█████ ████ ████ ████  ██  ███ 
-  ██  ██    ██  ██ ██ ██ █   
-  ██  ███   ██  ████  ██  ███ 
-  ██  ██    ██  ██ ██ ██     █
-  ██  ████  ██  ██ ██ ██  ███ 
+pub const BIG_TETRIS: &str = r#"████ ████ ████ ████  ██  ███ 
+ ██  ██    ██  ██ ██ ██ █   
+ ██  ███   ██  ████  ██  ███ 
+ ██  ██    ██  ██ ██ ██     █
+ ██  ████  ██  ██ ██ ██  ███ 
 "#;
 
 pub const BIG_TEXT_PAUSED: &str = r#" ██████  ██████  ██  ██    ████    ██████  ████   
@@ -64,18 +67,21 @@ fn draw_text(text: &str, area: &Rect, buf: &mut Buffer, style: &Style) {
     Paragraph::new(text).style(*style).render(msg_area, buf);
 }
 
-fn draw_board(game: &Game, area: &Rect, buf: &mut Buffer, width: u16, height: u16) {
-    let [game_area] = Layout::horizontal([Constraint::Length(width)])
-        .flex(Flex::Center)
-        .areas(*area);
-    let [game_area] = Layout::vertical([Constraint::Length(height)])
-        .flex(Flex::Center)
-        .areas(game_area);
+fn draw_board(game: &Game, board_area: &Rect, buf: &mut Buffer) {
+    Block::bordered()
+        .border_set(border::THICK)
+        .render(*board_area, buf);
+    let inner_area = Rect::new(
+        board_area.x + UNIT_X,
+        board_area.y + UNIT_Y,
+        UNIT_X * BOARD_WIDTH,
+        UNIT_X * BOARD_HEIGHT,
+    );
     let col_constraints = (0..BOARD_WIDTH as usize).map(|_| Constraint::Length(UNIT_X));
     let row_constraints = (0..BOARD_HEIGHT as usize).map(|_| Constraint::Length(UNIT_Y));
     let horizontal = Layout::horizontal(col_constraints).spacing(0);
     let vertical = Layout::vertical(row_constraints).spacing(0);
-    let rows = vertical.split(game_area);
+    let rows = vertical.split(inner_area);
     for (y, row) in rows.iter().enumerate() {
         let cols = horizontal.split(*row);
         for (x, cell) in cols.iter().enumerate() {
@@ -95,29 +101,49 @@ fn draw_board(game: &Game, area: &Rect, buf: &mut Buffer, width: u16, height: u1
     }
 }
 
+fn create_layout(area: &Rect) -> [Rect; 3] {
+    let board_width = UNIT_X * (BOARD_WIDTH + 2);
+    let board_height = UNIT_Y * (BOARD_HEIGHT + 2);
+    let stats_width = UNIT_X * STAT_WIDTH;
+    let [outer] = Layout::horizontal([Constraint::Length(board_width + stats_width)])
+        .flex(Flex::Center)
+        .areas(*area);
+
+    let [top_pane, bottom_pane] = Layout::vertical([
+        Constraint::Length(TITLE_HEIGHT),
+        Constraint::Length(board_height),
+    ])
+    .flex(Flex::Center)
+    .areas(outer);
+    let [title_area] = Layout::horizontal([Constraint::Length(TITLE_WIDTH)])
+        .flex(Flex::Center)
+        .areas(top_pane);
+    let [board_area, right_pane] = Layout::horizontal([
+        Constraint::Length(board_width),
+        Constraint::Length(stats_width),
+    ])
+    .areas(bottom_pane);
+    let stat_area = Rect::new(
+        right_pane.x,
+        right_pane.y,
+        stats_width,
+        STAT_HEIGHT * UNIT_Y,
+    );
+    [title_area, board_area, stat_area]
+}
+
 impl Widget for &Game {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let width = UNIT_X * BOARD_WIDTH;
-        let height = UNIT_Y * BOARD_HEIGHT;
-        let [game_border_area] = Layout::horizontal([Constraint::Length(width + 6)])
-            .flex(Flex::Center)
-            .areas(area);
-        let [game_border_area] = Layout::vertical([Constraint::Length(height + 4)])
-            .flex(Flex::Center)
-            .areas(game_border_area);
-        let title = Line::from(" Tetris Game ".bold());
-        let instructions = Line::from(vec![
-            " Quit".into(),
-            "<q> ".blue().bold(),
-            "Start/Pause".into(),
-            "<Esc> ".blue().bold(),
-        ]);
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
-        block.render(game_border_area, buf);
-        draw_board(self, &area, buf, width, height);
+        let [title_area, board_area, stat_area] = create_layout(&area);
+        // let instructions = Line::from(vec![
+        //     " Quit".into(),
+        //     "<q> ".blue().bold(),
+        //     "Start/Pause".into(),
+        //     "<Esc> ".blue().bold(),
+        // ]);
+        Paragraph::new(BIG_TETRIS).render(title_area, buf);
+        draw_board(self, &board_area, buf);
+        Block::bordered().render(stat_area, buf);
         match self.game_state {
             GameState::Playing => {}
             GameState::Paused => {
