fe1a2ca8318a23d89adc0fd7bbb2e9848630ddf8
tdobbel <thomas.dobbelaere@uclouvain.be>
2025-11-21 16:06:56 +0100

    Rust implementation

---
+++ sudoku/src/main.rs
@@ -0,0 +1,134 @@
+use rand::seq::SliceRandom;
+
+pub struct Sudoku {
+    grid: [[u8; 9]; 9],
+    entropy: [[[bool; 9]; 9]; 9],
+}
+
+impl Sudoku {
+    pub fn ispossible(&self, i: usize, j: usize, num: u8) -> bool {
+        for k in 0..9 {
+            if self.grid[i][k] == num || self.grid[k][j] == num {
+                return false;
+            }
+            let i0 = 3 * (i / 3);
+            let j0 = 3 * (j / 3);
+            for ik in 0..3 {
+                for jk in 0..3 {
+                    if self.grid[i0 + ik][j0 + jk] == num {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    }
+
+    pub fn empty() -> Self {
+        Sudoku {
+            grid: Default::default(),
+            entropy: [[[true; 9]; 9]; 9],
+        }
+    }
+
+    pub fn isfull(&self) -> bool {
+        for i in 0..9 {
+            for j in 0..9 {
+                if self.grid[i][j] == 0 {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+
+    pub fn min_entropy_cell(&self) -> Option<(usize, usize)> {
+        let mut res = None;
+        let mut vmin = 10;
+        for i in 0..9 {
+            for j in 0..9 {
+                if self.grid[i][j] > 0 {
+                    continue;
+                }
+                let entro = self.entropy[i][j].iter().filter(|&x| *x).count();
+                if entro < vmin {
+                    vmin = entro;
+                    res = Some((i, j));
+                }
+            }
+        }
+        res
+    }
+
+    pub fn remove_entry(&mut self, i: usize, j: usize) {
+        let num = self.grid[i][j];
+        if num == 0 {
+            return;
+        }
+        self.grid[i][j] = 0;
+        let num_indx = (num - 1) as usize;
+        for k in 0..9 {
+            self.entropy[i][k][num_indx] = self.ispossible(i, k, num);
+            self.entropy[k][j][num_indx] = self.ispossible(k, j, num);
+        }
+        let i0 = 3 * (i / 3);
+        let j0 = 3 * (j / 3);
+        for ik in 0..3 {
+            for jk in 0..3 {
+                self.entropy[i0 + ik][j0 + jk][num_indx] = self.ispossible(i0 + ik, j0 + jk, num);
+            }
+        }
+    }
+
+    pub fn set_entry(&mut self, i: usize, j: usize, num: u8) {
+        self.grid[i][j] = num;
+        let num_indx = (num - 1) as usize;
+        for k in 0..9 {
+            self.entropy[i][k][num_indx] = false;
+            self.entropy[k][j][num_indx] = false;
+        }
+        let i0 = 3 * (i / 3);
+        let j0 = 3 * (j / 3);
+        for ik in 0..3 {
+            for jk in 0..3 {
+                self.entropy[i0 + ik][j0 + jk][num_indx] = false;
+            }
+        }
+    }
+
+    pub fn display(&self) {
+        for row in self.grid.iter() {
+            for num in row.iter() {
+                print!("{num} ");
+            }
+            println!();
+        }
+    }
+}
+
+pub fn random_fill(sudo: &mut Sudoku) {
+    let (i, j) = match sudo.min_entropy_cell() {
+        Some((row, col)) => (row, col),
+        None => return,
+    };
+    let mut nums: Vec<u8> = (0..9)
+        .filter(|&k| sudo.entropy[i][j][k])
+        .map(|n| (n + 1) as u8)
+        .collect();
+    let mut rng = rand::rng();
+    nums.shuffle(&mut rng);
+    for num in nums.iter() {
+        sudo.set_entry(i, j, *num);
+        random_fill(sudo);
+        if sudo.isfull() {
+            return;
+        }
+        sudo.remove_entry(i, j);
+    }
+}
+
+fn main() {
+    let mut sudo = Sudoku::empty();
+    random_fill(&mut sudo);
+    sudo.display();
+}
